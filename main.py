d176ea967ff4b1bc43fe63e97856beb117449027
Ronen A <dr.robostorm@gmail.com>
2023-04-14 14:55:01 +0300

    Remove the unfinished 'meson/run.py' from 'main.py'

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ importlib.import_module('Meson')
 
 from .meson.compile import MesonCompileCommand
 from .meson.panel import MesonClearPanelCommand, MesonTogglePanelCommand
-from .meson.run import MesonRunCommand
 from .meson.setup import MesonSetupCommand
 
 __all__ = [
 	"MesonCompileCommand",
 	"MesonClearPanelCommand",
 	"MesonTogglePanelCommand",
-	"MesonRunCommand",
 	"MesonSetupCommand",
 ]
