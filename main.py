91ced8b1a79670bf9ff32b68b59afc64cb402ea5
Ronen A <dr.robostorm@gmail.com>
2023-07-24 16:43:13 +0300

    Minor changes
    Remove usage of `importlib` in 'main.py'.
    Add a list of escape sequences to 'meson-grammar.txt'.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import importlib
-importlib.import_module('Meson')
-
 from .meson.compile import MesonCompileCommand
 from .meson.panel import MesonClearPanelCommand, MesonTogglePanelCommand
 from .meson.setup import MesonSetupCommand
