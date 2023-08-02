1debae076bc76270e5a102a7d68382b73c81ad2c
Ronen A <dr.robostorm@gmail.com>
2023-08-02 13:40:46 +0300

    Organize some pointless stuff

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from .meson.compile import MesonCompileCommand
 from .meson.panel import MesonClearPanelCommand, MesonTogglePanelCommand
+
+from .meson.compile import MesonCompileCommand
 from .meson.setup import MesonSetupCommand
 
 __all__ = [
-	"MesonCompileCommand",
 	"MesonClearPanelCommand",
 	"MesonTogglePanelCommand",
+	
+	"MesonCompileCommand",
 	"MesonSetupCommand",
 ]
